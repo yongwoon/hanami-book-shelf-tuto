64cef78d47a34365c49d56327cc17b8e314fbe6a
yongwoon <yonyon.japan@gmail.com>
2020-01-25 19:34:42 +0900

    complete book list

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe 'List books' do
   before do
     repository.clear
 
-    repository.create(title: 'PoEAA', author: 'Martin Flowler')
-    repository.create(title: 'TDD', author: 'Kent Benk')
+    repository.create(title: 'PoEAA', author: 'Martin Fowler')
+    repository.create(title: 'TDD', author: 'Kent Beck')
   end
 
   it 'displays each book on the page' do
-    visit 'books'
+    visit '/books'
 
     within '#books' do
       expect(page).to have_css('.book', count: 2)
