d6b644d1d59826f020f68b818a674a2e46929e18
yongwoon <yonyon.japan@gmail.com>
2020-01-25 23:02:53 +0900

    Securing Our Form With Validations

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -4,10 +4,23 @@ module Web
       class Create
         include Web::Action
 
-        def call(params)
-          BookRepository.new.create(params[:book])
+        expose :book
 
-          redirect_to '/books'
+        params do
+          required(:book).schema do
+            required(:title).filled(:str?)
+            required(:author).filled(:str?)
+          end
+        end
+
+        def call(params)
+          if params.valid?
+            BookRepository.new.create(params[:book])
+
+            redirect_to '/books'
+          else
+            self.status = 422
+          end
         end
       end
     end
